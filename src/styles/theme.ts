b675f81e048b693ff685aea63f8c0dd4e712de2d
Saymon Damásio <saymon.damasio95@gmail.com>
2021-12-21 15:37:45 -0300

    feat: add colors to the theme of chakra-ui

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,11 +1,27 @@
 import { extendTheme } from '@chakra-ui/react'
 
+const colors = {
+  gray: {
+    '900': '#181B23',
+    '800': '#1F2029',
+    '700': '#353646',
+    '600': '#4B4D63',
+    '500': '#616480',
+    '400': '#797D9A',
+    '300': '#9699B0',
+    '200': '#B3B5C6',
+    '100': '#D1D2DC',
+    '50': '#EEEEF2',
+  },
+}
+
 export const theme = extendTheme({
   styles: {
+    colors,
     global: {
       body: {
-        backgroundColor: 'gray.900',
-        color: 'gray.50',
+        backgroundColor: colors.gray['900'],
+        color: colors.gray['50'],
       },
     },
   },
