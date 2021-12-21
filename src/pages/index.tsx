d3fb9f09471f2ab75755714e8543ef40959cce3c
Saymon Damásio <saymon.damasio95@gmail.com>
2021-12-21 17:24:46 -0300

    feat: add page signin

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,57 @@
-import type { NextPage } from 'next'
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+} from '@chakra-ui/react'
 
-const Home: NextPage = () => {
-  return <p>Hello</p>
+export default function Home() {
+  return (
+    <Flex w="100vw" h="100vh" alignItems="center" justifyContent="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p={8}
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing={4}>
+          <FormControl>
+            <FormLabel htmlFor="email">Email</FormLabel>
+            <Input
+              id="email"
+              name="email"
+              type="email"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{ bgColor: 'gray.900' }}
+              size="lg"
+            />
+          </FormControl>
+
+          <FormControl>
+            <FormLabel htmlFor="password">Senha</FormLabel>
+            <Input
+              id="password"
+              name="password"
+              type="password"
+              focusBorderColor="pink.500"
+              bgColor="gray.900"
+              variant="filled"
+              _hover={{ bgColor: 'gray.900' }}
+              size="lg"
+            />
+          </FormControl>
+        </Stack>
+        <Button type="submit" mt={6} colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  )
 }
-
-export default Home
