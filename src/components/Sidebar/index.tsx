020d64be92274f0f726e8a2640d48451949b1be9
Saymon Damásio <saymon.damasio95@gmail.com>
2021-12-22 17:11:12 -0300

    feat: create nav section component

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,56 +1,47 @@
 import { Box, Icon, Link, Stack, Text } from '@chakra-ui/react'
+import { title } from 'process'
 import {
   RiContactsLine,
   RiDashboardLine,
   RiGitMergeLine,
   RiInputMethodLine,
 } from 'react-icons/ri'
+import { NavSection } from './NavSection'
 
 export function Sidebar() {
   return (
     <Box as="aside" w="64" mr="8">
       <Stack spacing="12" align="flex-start">
-        <Box>
-          <Text fontWeight="bold" color="gray.400" fontSize="small">
-            GERAL
-          </Text>
-          <Stack spacing="4" mt="8">
-            <Link display="flex" alignItems="center">
-              <Icon as={RiDashboardLine} fontSize={20} />
-              <Text ml="4" fontWeight="medium">
-                Dashboard
-              </Text>
-            </Link>
+        <NavSection title="GERAL">
+          <Link display="flex" alignItems="center">
+            <Icon as={RiDashboardLine} fontSize={20} />
+            <Text ml="4" fontWeight="medium">
+              {title}
+            </Text>
+          </Link>
 
-            <Link display="flex" alignItems="center">
-              <Icon as={RiContactsLine} fontSize={20} />
-              <Text ml="4" fontWeight="medium">
-                Usuários
-              </Text>
-            </Link>
-          </Stack>
-        </Box>
+          <Link display="flex" alignItems="center">
+            <Icon as={RiContactsLine} fontSize={20} />
+            <Text ml="4" fontWeight="medium">
+              Usuários
+            </Text>
+          </Link>
+        </NavSection>
+        <NavSection title="AUTOMAÇÃO">
+          <Link display="flex" alignItems="center">
+            <Icon as={RiInputMethodLine} fontSize={20} />
+            <Text ml="4" fontWeight="medium">
+              Formulários
+            </Text>
+          </Link>
 
-        <Box>
-          <Text fontWeight="bold" color="gray.400" fontSize="small">
-            AUTOMAÇÃO
-          </Text>
-          <Stack spacing="4" mt="8">
-            <Link display="flex" alignItems="center">
-              <Icon as={RiInputMethodLine} fontSize={20} />
-              <Text ml="4" fontWeight="medium">
-                Formulários
-              </Text>
-            </Link>
-
-            <Link display="flex" alignItems="center">
-              <Icon as={RiGitMergeLine} fontSize={20} />
-              <Text ml="4" fontWeight="medium">
-                Automação
-              </Text>
-            </Link>
-          </Stack>
-        </Box>
+          <Link display="flex" alignItems="center">
+            <Icon as={RiGitMergeLine} fontSize={20} />
+            <Text ml="4" fontWeight="medium">
+              Automação
+            </Text>
+          </Link>
+        </NavSection>
       </Stack>
     </Box>
   )
