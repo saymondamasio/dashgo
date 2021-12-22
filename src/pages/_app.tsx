42855d6a1db87cfbed63c802ae4a1a9a336aebbc
Saymon Damásio <saymon.damasio95@gmail.com>
2021-12-22 19:12:35 -0300

    feat: add responsiveness to sidebar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import { SidebarDrawerContextProvider } from '../contexts/SidebarDrawerContext'
 import { theme } from '../styles/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
+      <SidebarDrawerContextProvider>
+        <Component {...pageProps} />
+      </SidebarDrawerContextProvider>
     </ChakraProvider>
   )
 }
