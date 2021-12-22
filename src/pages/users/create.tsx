7ce2d8240392971ff7154426c9bd17abe63a1667
Saymon Damásio <saymon.damasio95@gmail.com>
2021-12-22 09:46:23 -0300

    feat: add user create page

---
+++ src/pages/users/create.tsx
@@ -0,0 +1,57 @@
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  Heading,
+  HStack,
+  SimpleGrid,
+  VStack,
+} from '@chakra-ui/react'
+import { RiAddLine, RiPencilLine } from 'react-icons/ri'
+import { Sidebar } from '../../components/Sidebar'
+import { Header } from '../../components/Header'
+import { Pagination } from '../../components/Pagination'
+import { Input } from '../../components/Form/Input'
+
+export default function CreateUser() {
+  return (
+    <Box>
+      <Header />
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
+
+        <Box flex="1" borderRadius="8" bgColor="gray.800" p="8">
+          <Heading size="lg" fontWeight="normal">
+            Criar usuário
+          </Heading>
+
+          <Divider my="6" borderColor="gray.700" />
+
+          <VStack spacing="8">
+            <SimpleGrid minChildWidth={240} spacing="8" w="100%">
+              <Input name="name" type="text" label="Nome Completo" />
+              <Input name="email" type="email" label="Email" />
+            </SimpleGrid>
+
+            <SimpleGrid minChildWidth={240} spacing="8" w="100%">
+              <Input name="password" type="password" label="Senha" />
+              <Input
+                name="password_conformation"
+                type="password"
+                label="Confirmação da senha"
+              />
+            </SimpleGrid>
+          </VStack>
+
+          <Flex mt="8" justify="flex-end">
+            <HStack spacing="4">
+              <Button colorScheme="whiteAlpha">Cancelar</Button>
+              <Button colorScheme="pink">Salvar</Button>
+            </HStack>
+          </Flex>
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
