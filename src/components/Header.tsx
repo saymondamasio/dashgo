e4f28463270659377eaf865771609c9a3a76f2dd
Saymon Damásio <saymon.damasio95@gmail.com>
2021-12-22 17:02:02 -0300

    feat: add search box component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,6 +13,7 @@ import {
 import { RiNotificationLine, RiSearchLine, RiUserAddLine } from 'react-icons/ri'
 import { NotificationsNav } from './Header/NotificationsNav'
 import { Profile } from './Header/Profile'
+import { SearchBox } from './Header/SearchBox'
 
 export function Header() {
   return (
@@ -33,32 +34,7 @@ export function Header() {
         </Text>
       </Text>
 
-      <InputGroup
-        ml="6"
-        alignSelf="center"
-        flex="1"
-        color="gray.200"
-        bgColor="gray.800"
-        borderRadius="full"
-        py="4"
-        maxWidth={400}
-      >
-        <Input
-          pl="8"
-          px="4"
-          mr="4"
-          color="gray.50"
-          variant="unstyled"
-          placeholder="Buscar na plataforma"
-          _placeholder={{ color: 'gray.400' }}
-        />
-        <InputRightElement
-          mr="4"
-          h="100%"
-          pointerEvents="none"
-          children={<RiSearchLine fontSize="20" color="gray.300" />}
-        />
-      </InputGroup>
+      <SearchBox />
 
       <Flex align="center" ml="auto">
         <NotificationsNav />
