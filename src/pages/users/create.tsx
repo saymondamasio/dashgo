b55aa78f3a74ff2394b0bdbf70e574dcc6facf38
Saymon Damásio <saymon.damasio95@gmail.com>
2021-12-22 21:15:18 -0300

    feat: add responsiveness to create user page

--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -21,7 +21,7 @@ export default function CreateUser() {
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
 
-        <Box flex="1" borderRadius="8" bgColor="gray.800" p="8">
+        <Box flex="1" borderRadius="8" bgColor="gray.800" p={['6', '8']}>
           <Heading size="lg" fontWeight="normal">
             Criar usuário
           </Heading>
@@ -29,12 +29,12 @@ export default function CreateUser() {
           <Divider my="6" borderColor="gray.700" />
 
           <VStack spacing="8">
-            <SimpleGrid minChildWidth={240} spacing="8" w="100%">
+            <SimpleGrid minChildWidth={240} spacing={['6', '8']} w="100%">
               <Input name="name" type="text" label="Nome Completo" />
               <Input name="email" type="email" label="Email" />
             </SimpleGrid>
 
-            <SimpleGrid minChildWidth={240} spacing="8" w="100%">
+            <SimpleGrid minChildWidth={240} spacing={['6', '8']} w="100%">
               <Input name="password" type="password" label="Senha" />
               <Input
                 name="password_conformation"
